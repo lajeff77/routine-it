d21cbcd140c1066dcece702a17a494886eeba891
Lauryn <lajeff77@gmail.com>
2024-03-25 14:29:25 -0700

    updated hero section to use mui

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,13 +1,20 @@
+import { Box, Button, Typography  } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom'
-
 
 const HeroSection = () => {
     return(
-        <div className='hero-section'>
-            <h1>Everyone should be able to keep a routine!</h1>
-            <Link to='/signup'><button>Get Started!</button></Link>
-        </div>
+        <Box sx={{height: "700px",
+         background: "linear-gradient(0deg, rgba(255, 255, 255, 0.45), rgba(0, 0, 0, 0.45)), url('/bathroom-counter-medium.jpg')", 
+         backgroundRepeat: "no-repeat", 
+         backgroundSize: "cover", 
+         color: "white",
+          padding: "5%", 
+          display: "flex", 
+          flexDirection: "column", 
+          alignItems: "center"}}>
+            <Typography variant="h1" sx={{textAlign: "center", paddingTop: "150px", paddingBottom: "50px"}} >Everyone should be able to keep a routine!</Typography>
+            <Button href='/signup' variant="contained" disableElevation sx={{margin: "10px"}}>Get Started!</Button>
+        </Box>
     )
 }
 
--- client/src/components/MainHeader.js
+++ client/src/components/MainHeader.js
@@ -1,6 +1,5 @@
 import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 const MainHeader = () => {
 
