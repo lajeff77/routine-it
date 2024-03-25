07ab4099a183644f34656dbf18cdbb2658a23a5d
Lauryn <lajeff77@gmail.com>
2024-03-25 13:35:32 -0700

    updating landing page summary to mui, minor style updates for main header

--- client/src/components/MainHeader.js
+++ client/src/components/MainHeader.js
@@ -5,9 +5,9 @@ import {Link} from 'react-router-dom';
 const MainHeader = () => {
 
     return(
-        <AppBar position="static">
+        <AppBar position="static" sx={{padding: "1%"}}>
             <Toolbar>
-                <Typography variant='h6' component="div" sx={{ flexGrow: 1 }}>Routine It</Typography>            
+                <Typography variant='h3' component="div" sx={{ flexGrow: 1 }}>Routine It</Typography>            
                 <Button href="/" variant="contained" disableElevation sx={{margin: "10px"}}>Home</Button>
                 <Button href="/signup" variant="contained" disableElevation sx={{margin: "10px"}}>Sign Up</Button>
                 <Button href="/signin" variant="contained" disableElevation sx={{margin: "10px"}}>Sign In</Button>
