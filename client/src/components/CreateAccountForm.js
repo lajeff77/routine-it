2af67ce789e0bb23c101100dde2409ac3a848c56
Lauryn <lajeff77@gmail.com>
2024-04-10 17:07:30 -0700

    added in local api calls for create account and login

--- client/src/components/CreateAccountForm.js
+++ client/src/components/CreateAccountForm.js
@@ -1,15 +1,33 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import {Navigate} from 'react-router-dom';
 import {Box, Link, Button, Grid, TextField, Typography } from '@mui/material';
 
 const CreateAccountForm = () => {
 
-    const [user, setUser] = useState(false);
-    const handleSubmit = (e) => {
+    const [authenticated, setAuthenticated] = useState(false);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log(`Sending ${password} as password`);
+        console.log(`JSON string will be ${JSON.stringify({firstName, lastName, email, password})}`);
         /*TODO: call account creation api here */
-        console.log("Created account");
-        setUser(true);
+        try {
+            const response = await axios.post("http://localhost:8080/api/v1/user/create", {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            });
+            console.log(`New user with id ${response?.id}.`);
+            setAuthenticated(true);  
+        } catch (error) {
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     return (
@@ -18,14 +36,14 @@ const CreateAccountForm = () => {
             <Typography variant='h2' align='center' sx={{margin:"20px"}}>Sign up for Routine It</Typography>
             <Typography variant='body1' sx={{margin:"20px"}}>Already have an account? Sign in <Link href="/signin">here</Link>.</Typography>
             <Box component="form" onSubmit={handleSubmit} display="flex" flexDirection="column" alignItems="center">
-                    <TextField required type="text" name="first-name" label="First Name" sx={{margin:"10px"}} />
-                    <TextField required type="text" name="last-name" label="Last Name" sx={{margin:"10px"}} />
-                    <TextField required type="email" name="email" label="Email" sx={{margin:"10px"}}/>
-                    <TextField required type="password" name="password" label="Password" sx={{margin:"10px"}}/>
+                    <TextField required type="text" name="first-name" label="First Name" sx={{margin:"10px"}} onChange={(e) => setFirstName(e.target.value)} />
+                    <TextField required type="text" name="last-name" label="Last Name" sx={{margin:"10px"}} onChange={(e) => setLastName(e.target.value)}/>
+                    <TextField required type="email" name="email" label="Email" sx={{margin:"10px"}} onChange={(e) => setEmail(e.target.value)}/>
+                    <TextField required type="password" name="password" label="Password" sx={{margin:"10px"}} onChange={(e) => setPassword(e.target.value)}/>
                 <Button type="submit" variant="contained" sx={{margin:"5px"}}>Create Account</Button>
             </Box>
             </Grid>
-            {user && <Navigate to="/mydashboard" replace={true} />}
+            {authenticated && <Navigate to="/mydashboard" replace={true} />}
         </Box>
     )
 }
--- client/src/components/SignInForm.js
+++ client/src/components/SignInForm.js
@@ -1,16 +1,33 @@
-import React, {useState} from "react";
+import React, { useState} from 'react';
+import axios from 'axios';
 import {Navigate} from 'react-router-dom';
 import {Box, Link, Button, Grid, TextField, Typography } from '@mui/material';
 
 const SignInForm = () => {
 
-    const [user, setUser] = useState(false);
-    const handleSubmit = (e) => {
+    const [authenticated, setAuthenticated] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        /*TODO: call account login api here */
-        console.log("Signed in account");
-        setUser(true);
-    }
+        try {
+            const response = await axios.post("http://localhost:8080/api/v1/user/login", {
+            email: email,
+            password: password,
+            });
+            if(response.status !== 200){
+                throw new Error("Authentication failed (status: " + response.status);
+            }
+            else{
+                console.log(`Request is ok. Status is ${response.status}: ${response.message}.`);
+                setAuthenticated(true);
+            }
+        } catch (error) {
+            console.error(`An error occurred: ${error.message}`);
+
+        }
+    };
 
     return(
         <Box display="flex" justifyContent="center" sx={{margin:"100px"}}>
@@ -18,12 +35,12 @@ const SignInForm = () => {
             <Typography variant='h2' align='center' sx={{margin:"20px"}}>Welcome Back to Routine It</Typography>
             <Typography variant='body1' sx={{margin:"20px"}}>Need to create an account? Sign up <Link href="/signup">here</Link>.</Typography>
             <Box component="form" onSubmit={handleSubmit} display="flex" flexDirection="column" alignItems="center">
-                    <TextField required type="email" name="email" label="Email" sx={{margin:"10px"}}/>
-                    <TextField required type="password" name="password" label="Password" sx={{margin:"10px"}}/>
+                <TextField required type="email" name="email" label="Email" onChange={(e) => setEmail(e.target.value)} sx={{margin:"10px"}}/>
+                <TextField required type="password" name="password" label="Password" onChange={(e) => setPassword(e.target.value)} sx={{margin:"10px"}}/>
                 <Button type="submit" variant="contained" sx={{margin:"5px"}}>Sign In</Button>
             </Box>
             </Grid>
-            {user && <Navigate to="/mydashboard" replace={true} />}
+            {authenticated && <Navigate to="/mydashboard" replace={true} />}
         </Box>
     )
 
