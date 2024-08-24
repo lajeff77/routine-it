d0576010b38716d4f460eed32b8410619a004098
lajeff77 <lajeff77@gmail.com>
2024-08-24 10:32:48 -0600

    somewhat working routine checklists that parse from the service response

---
+++ client/src/components/RoutineCheckListPage/RoutineCheckList.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+import {Box, Checkbox, FormGroup, FormControlLabel, FormControl, FormLabel, Typography} from "@mui/material";
+
+const RoutineCheckList = (props) => {
+    const [checked, setChecked] = useState(() => {
+        const initialCheckedState = {};
+        Array.from(props.tasks).forEach(task => {
+            initialCheckedState[task.name] = task.complete;
+        });
+        return initialCheckedState;
+    });
+
+    const handleChange = (event) =>
+    {
+        console.log(`Set ${event.target.name} = ${event.target.checked}`);
+        setChecked({...checked, [event.target.name]: event.target.checked});
+        //make api call here
+    }
+
+    return(
+        <Box>
+            <Typography variant="h2" sx={{textAlign:"center"}}>{props.name}</Typography>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">Tasks</FormLabel>
+                <FormGroup>
+                    {Array.from(props.tasks).map((task) => (
+                        <FormControlLabel
+                            key={task.id}
+                            control={<Checkbox checked={checked[task.name]} onChange={handleChange} name={task.name} />}
+                            label={task.name} 
+                        />
+                    ))}
+                </FormGroup>
+            </FormControl>
+        </Box>
+    )
+}
+
+export default RoutineCheckList;
--- client/src/pages/RoutineChecklistPage.js
+++ client/src/pages/RoutineChecklistPage.js
@@ -1,29 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {Box} from "@mui/material";
-import RoutineChecklistHeader from "../components/RoutineChecklistHeader";
+import {Box, Typography} from "@mui/material";
+import RoutineChecklistHeader from "../components/RoutineCheckListPage/RoutineChecklistHeader";
+import RoutineCheckList from "../components/RoutineCheckListPage/RoutineCheckListGroup";
 
 const RoutineChecklistPage = () => { 
 
     const [routine,setRoutine] = useState({});
-    React.useEffect(() => {
+    const [routineId, setRoutineId] = useState(0);
+    const [highEffortTasks, setHighEffortTasks] = useState([]);
+    const [medEffortTasks, setMedEffortTasks] = useState([]);
+    const [lowEffortTasks, setLowEffortTasks] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
    
-          
-        axios.get('http://localhost:8080/api/v1/routines/1')
-        .then(function (response) {
-            // handle success
-            console.log(response);
-            setRoutine(response.data);
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
+        //get the routine and id to get the tasks
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/v1/routines/1')
+            
+                // handle success
+                // console.log(response);
+                setRoutine(response.data);
+                setRoutineId(response.data.id);
+
+                // query tasks by routine id
+                const taskResponse = await axios.get(`http://localhost:8080/api/v1/tasks/${response.data.id}`);
+         
+                //sort tasks
+                // console.log(taskResponse); 
+                var taskResp = taskResponse.data;
+                for(let i = 0; i < taskResp.length; i++)
+                {
+                    if(taskResp[i].highEffort)
+                        await setHighEffortTasks(highEffortTasks => [...highEffortTasks, taskResp[i]]);
+                    if(taskResp[i].mediumEffort)
+                        await setMedEffortTasks(medEffortTasks => [...medEffortTasks, taskResp[i]]);
+                    if(taskResp[i].lowEffort)
+                        await setLowEffortTasks(lowEffortTasks => [...lowEffortTasks, taskResp[i]]);
+                }
+                await setLoaded(true);
+            } catch (error) {
+                console.error('Error fetching tasks:', error);
+            }
+        };
+
+        fetchData();
     },[])
+
+    const filterUniqueTasks = (array) => {
+        const uniqueTasks = Array.from(
+            new Map(Array.from(array).map(task => [task.name, task])).values()
+        );
+        return uniqueTasks;
+    }
     
     return(
         <Box>
-            <RoutineChecklistHeader name={routine.name}/>
+            <RoutineChecklistHeader name={routine.name || "Loading..."}/>
+            { loaded ? <RoutineCheckList routineId={routineId} highEffortTasks={filterUniqueTasks(highEffortTasks)} medEffortTasks={filterUniqueTasks(medEffortTasks)} lowEffortTasks={filterUniqueTasks(lowEffortTasks)}/> : <Box><Typography variant="h2" sx={{textAlign: "center"}}>Loading...</Typography></Box> }
         </Box>
     )
 }
